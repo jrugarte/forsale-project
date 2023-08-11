89f60d37d67c17a8a541eeb1f9af8cbe1e8ff9c8
Oscar Ugarte <130696830+jrugarte@users.noreply.github.com>
2023-08-11 12:54:48 -0600

    pd open and close

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -6,11 +6,17 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children})=>{
     const [count, setCount] = useState(0)
+    const [isPDOpen, setIsPDOpen] = useState(false)
+    const openPD = () => setIsPDOpen(true)
+    const closePD = () => setIsPDOpen(false)
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
+            openPD,
+            closePD,
+            isPDOpen
         }}>
             {children}
         </ShoppingCartContext.Provider>
