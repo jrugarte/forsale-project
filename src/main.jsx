f3c04794fb7af2ac2ced1b0c6f429785dab1d337
Oscar Ugarte <130696830+jrugarte@users.noreply.github.com>
2023-09-11 15:07:03 -0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '/Users/junior/Documents/Proyectos/vite-tailwind-react/vtr-project/src/Pages/app/index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Pages/app";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
